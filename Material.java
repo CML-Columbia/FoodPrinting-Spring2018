a685a8eb215fe9d621da4f1cf3bb8808614dcb1f
sarahyuan02 <sxy2003@columbia.edu>
2018-04-20 13:19:16 -0400

    Pickup and drop off material implementation

--- Material.java
+++ Material.java
@@ -8,32 +8,34 @@ public class Material {
 	int materialNumber;
 	double E_curr;
 	double extrusionMultiplier;
-	static HashMap<Integer, double[]> coordinates = initMap();
+	private static HashMap<Integer, double[]> coordinates;
 
 
-	private static HashMap<Integer, double[]> initMap(){
-		HashMap<Integer, double[]> coords = new HashMap<Integer, double[]>();
+	private static void initMap(){
+		coordinates = new HashMap<Integer, double[]>();
 		coordinates.put(1, new double[]{1.0, 0.0, 15.0});
 		coordinates.put(2, new double[]{46.0, 0.0, 15.0});
-		coordinates.put(3, new double[]{90.0, 0.0, 15.0});
+		coordinates.put(3, new double[]{89.0, 0.0, 15.0});
 		coordinates.put(4, new double[]{132.0, 0.0, 15.0});
 		coordinates.put(5, new double[]{175.0, 0.0, 15.0});
 		coordinates.put(6, new double[]{218.0, 0.0, 15.0});
 		coordinates.put(7, new double[]{261.0, 0.0, 15.0});
-		return coords;
 	}
 
 	public Material(int materialNumber, double extrusionMultiplier) {
 		this.materialNumber = materialNumber;
 		this.E_curr = 0.0;
 		this.extrusionMultiplier = extrusionMultiplier;
+		initMap();
 	}
 
-	private double[] getRackCoord(int matNum){
-		return coordinates.get(matNum);
+	//Tutch changed from private to public
+	//Tutch also got rid of the argument so that hashing with materialNumber directly
+	public double[] getRackCoord(){
+		return coordinates.get(materialNumber);
 	}
 
-	private void updateE_curr(double e){
+	public void updateE_curr(double e){
 		this.E_curr = e;
 
 	}
